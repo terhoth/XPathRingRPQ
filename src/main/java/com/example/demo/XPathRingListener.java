2ed99d69a9f2b086ab6be50c89ece9c06bf9228a
Terho Thorel <terho.thorel@tuni.fi>
2026-07-28 14:41:12 +0300

    add warnings to most cases where evaluation isn't implemented

--- src/main/java/com/example/demo/XPathRingListener.java
+++ src/main/java/com/example/demo/XPathRingListener.java
@@ -23,6 +23,7 @@ public class XPathRingListener extends xpathBaseListener {
     private boolean insideAttributeTest = false;
     private Object attributeValue;
 
+    private final boolean verbose = false;
 
     private Output output = new Output();
 
@@ -50,46 +51,79 @@ public class XPathRingListener extends xpathBaseListener {
 
     @Override 
     public void exitEqualityExpr(xpathParser.EqualityExprContext ctx) {
-	System.out.println("exitEqualityExpr");
-	if (ctx.getChildCount() > 1)
-	{
+	if (ctx.getChildCount() > 1) {
+	    if (this.verbose) System.out.println("exitEqualityExpr");
 	    this.queryStack.peek().append(" " + ctx.getChild(1) + " "); // operator
 	    this.queryStack.peek().append(this.attributeValue.toString());
+	    if (this.verbose) System.out.println(this.queryStack.peek());
+
+	    if (!insideAttributeTest) {
+		String warnMessage = "Equality expressions may only be used to test attributes";
+    		output.printWarning(warnMessage);
+	    }
 	}
-	System.out.println(this.queryStack.peek());
+	if (ctx.getChildCount() > 3) {
+	    String warnMessage = "Too many operands in equality expression";
+    	    output.printWarning(warnMessage);
+	}
     }
 
     @Override 
     public void exitRelationalExpr(xpathParser.RelationalExprContext ctx) {
-	System.out.println("exitRelationalExpr");
-	if (ctx.getChildCount() > 1)
-	{
+	if (ctx.getChildCount() > 1) {
+	    if (this.verbose) System.out.println("exitRelationalExpr");
 	    this.queryStack.peek().append(" " + ctx.getChild(1) + " "); // operator
 	    this.queryStack.peek().append(this.attributeValue.toString());
+	    if (this.verbose) System.out.println(this.queryStack.peek());
+	
+	    if (!insideAttributeTest) {
+		String warnMessage = "Relational expressions may only be used to test attributes";
+    		output.printWarning(warnMessage);
+	    }
 	}
-	System.out.println(this.queryStack.peek());
+	if (ctx.getChildCount() > 3) {
+	    String warnMessage = "Too many operands in relational expression";
+    	    output.printWarning(warnMessage);
+	}
+    }
+
+    @Override
+    public void exitAdditiveExpr(xpathParser.AdditiveExprContext ctx) {
+	if (ctx.getChildCount() > 1) {
+	    String warnMessage = "Addition has not been implemented";
+    	    output.printWarning(warnMessage);
+	}
+    }
+
+    @Override
+    public void exitMultiplicativeExpr(xpathParser.MultiplicativeExprContext ctx) {
+	if (ctx.getChildCount() > 1) {
+	    String warnMessage = "Multiplication has not been implemented";
+    	    output.printWarning(warnMessage);
+	}
     }
 
     @Override
     public void exitPrimaryExpr(xpathParser.PrimaryExprContext ctx) {
-	System.out.println("exitPrimaryExpr");
+	if (this.verbose) System.out.println("exitPrimaryExpr");
     	//Checking if the child node is leaf node
     	if (ctx.getChild(0).getChild(0) == null) {
     		this.attributeValue = ctx.getChild(0);
-    	}
-	System.out.println(this.queryStack.peek());
+    	} else {
+	    String warnMessage = "Functions, variables and parenthesized expressions are unimplemented";
+    	    output.printWarning(warnMessage);
+	}	
+	if (this.verbose) System.out.println(this.queryStack.peek());
     }
 
     @Override
     public void enterRelativeLocationPath(xpathParser.RelativeLocationPathContext ctx) {
-	System.out.println("enterRelativeLocationPath");
 	this.firstStep = true;
-	System.out.println(this.queryStack.peek());
     }
 
     @Override
     public void exitRelativeLocationPath(xpathParser.RelativeLocationPathContext ctx) {
-	System.out.println("exitRelativeLocationPath");
+	if (this.verbose) System.out.println("exitRelativeLocationPath");
 	if (this.queryStack.size() == 1) {
 	    // this is the end of the main query
 	    int n_of_steps = (ctx.getChildCount() + 1) / 2;
@@ -103,12 +137,12 @@ public class XPathRingListener extends xpathBaseListener {
 		this.queryStack.peek().insert(i, " <");
 	    }
 	}
-	System.out.println(this.queryStack.peek());
+	if (this.verbose) System.out.println(this.queryStack.peek());
     }
 
     @Override
     public void exitAxisSpecifier(xpathParser.AxisSpecifierContext ctx) {
-	System.out.println("exitAxisSpecifier");
+	if (this.verbose) System.out.println("exitAxisSpecifier");
     	StringBuilder sb = new StringBuilder();
     	sb.append(ctx.getChild(0));
 
@@ -150,30 +184,33 @@ public class XPathRingListener extends xpathBaseListener {
     	}
 
 	this.queryStack.peek().append(this.startEdge.peek());
-	System.out.println(this.queryStack.peek());
+	if (this.verbose) System.out.println(this.queryStack.peek());
     }
 
     @Override
     public void enterStep(xpathParser.StepContext ctx) {
-	System.out.println("enterStep");
+	if (this.insideAttributeTest) {
+	    String warnMessage = "Attribute tests must consist of an attribute name, operator and value.";
+    	    output.printWarning(warnMessage);
+	}
 	if (!this.firstStep && this.queryStack.peek().charAt(this.queryStack.peek().length()-1) != ' ') {
+	    if (this.verbose) System.out.println("enterStep");
 	    this.queryStack.peek().append("/");
+	    if (this.verbose) System.out.println(this.queryStack.peek());
 	}
 	this.deletedStep = false;
-	System.out.println(this.queryStack.peek());
     }
 
     @Override
     public void exitStep(xpathParser.StepContext ctx) {
-	System.out.println("exitStep");
+	if (this.verbose) System.out.println("exitStep");
 	this.queryStack.peek().append(this.endEdge.peek());
 	
 	if (firstStep && this.queryStack.size() == 1) {
 	    // this is the very first vertex of the main query
 	    if (this.axis.peek() != "child")  {
 	        String warnMessage = "The first step should contain only a vertex name (or wildcard vertex)";
-    	        String details = this.queryStack.peek().toString();
-    	        output.printWarning(warnMessage, details);
+    	        output.printWarning(warnMessage);
 	    }
 	    if (this.queryStack.peek().toString().equals("<¤>")) {
 		// replace unspecified start vertex with variable
@@ -193,13 +230,12 @@ public class XPathRingListener extends xpathBaseListener {
 	        this.queryStack.peek().delete(i, this.queryStack.peek().length());
 		this.deletedStep = true;
 	    }
-	    // warn of nameless edges
+	    // warn about nameless edges
 	    uselessStep = "/" + this.startEdge.peek() + this.endEdge.peek();
 	    i = this.queryStack.peek().indexOf(uselessStep);
 	    if (i != -1) {
 	        String warnMessage = "Nameless edges can be costly in evaluation. Besides, they have not been implemented yet!";
-    	        String details = uselessStep;
-    	        output.printWarning(warnMessage, details);
+    	        output.printWarning(warnMessage);
 	    }
 	}
 
@@ -207,50 +243,64 @@ public class XPathRingListener extends xpathBaseListener {
 	this.startEdge.pop();
 	this.endEdge.pop();
 	this.firstStep = false;
-	System.out.println(this.queryStack.peek());
+	if (this.verbose) System.out.println(this.queryStack.peek());
     }
 
     @Override
     public void enterNameTest(xpathParser.NameTestContext ctx) {
-	System.out.println("enterNameTest");
+	if (this.verbose) System.out.println("enterNameTest");
 	this.nCName = "";
-	System.out.println(this.queryStack.peek());
+	if (this.verbose) System.out.println(this.queryStack.peek());
     }
 
     @Override
     public void exitNameTest(xpathParser.NameTestContext ctx) {
-	System.out.println("exitNameTest");
+	if (this.verbose) System.out.println("exitNameTest");
 	int n_of_steps = (ctx.parent.parent.parent.getChildCount() + 1) / 2;
-	// main query begins with a vertex, predicates with an edge
+	// main query begins with a vertex; subqueries (=predicates) begin with an edge
 	if ((this.queryStack.size() == 1 && n_of_steps % 2 == 1) ||
 	    (this.queryStack.size() >  1 && n_of_steps % 2 == 0))
 	{
 	    this.queryStack.peek().append('¤');
 	}
 	this.queryStack.peek().append(this.nCName);
-	System.out.println(this.queryStack.peek());
+	if (this.verbose) System.out.println(this.queryStack.peek());
     }
 
     @Override
     public void exitNCName(xpathParser.NCNameContext ctx) {
-	System.out.println("exitNameTest");
+	if (this.verbose) System.out.println("exitNameTest");
 	StringBuilder sb = new StringBuilder();
     	this.nCName = sb.append(ctx.getChild(0)).toString();	
-	System.out.println(this.queryStack.peek());
+	if (this.verbose) System.out.println(this.queryStack.peek());
     }
 
     @Override
     public void enterPredicate(xpathParser.PredicateContext ctx) {
-	System.out.println("enterPredicate");
+	if (this.verbose) System.out.println("enterPredicate");
+	if (this.firstStep && this.queryStack.size() == 1) {
+	    String warnMessage = "The first step should contain only a vertex name (or wildcard vertex)";
+    	    output.printWarning(warnMessage);
+	}	
+	int n_of_steps = (ctx.parent.parent.getChildCount() + 1) / 2;
+	if (this.insideAttributeTest) {
+	    String warnMessage = "Branching is not allowed on attributes";
+    	    output.printWarning(warnMessage);
+	}
+	else if ((this.queryStack.size() == 1 && n_of_steps % 2 == 0) ||
+	         (this.queryStack.size() >  1 && n_of_steps % 2 == 1)) {
+	    String warnMessage = "Branching is not allowed on edges";
+    	    output.printWarning(warnMessage);
+	}
 	this.firstStep = true;
 	this.insideAttributeTest = false;
 	this.queryStack.push(new StringBuilder());
-	System.out.println(this.queryStack.peek());
+	if (this.verbose) System.out.println(this.queryStack.peek());
     }
 
     @Override
     public void exitPredicate(xpathParser.PredicateContext ctx) {
-	System.out.println("exitPredicate");
+	if (this.verbose) System.out.println("exitPredicate");
 	StringBuilder predicate = this.queryStack.pop();
 	if (this.insideAttributeTest) {
 	    this.insideAttributeTest = false;
@@ -262,7 +312,7 @@ public class XPathRingListener extends xpathBaseListener {
 	    this.queryStack.peek().append(predicate.toString());
 	    this.queryStack.peek().append("}");
 	}
-	System.out.println(this.queryStack.peek());
+	if (this.verbose) System.out.println(this.queryStack.peek());
     }
 
 }
